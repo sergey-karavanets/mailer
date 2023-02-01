f2e15cbed3daa8a33798d7a98817e189b9f6eb8d
karavanets <dront22rus@mail.ru>
2023-02-01 15:04:45 +0700

    add func 'main' in main.py with smtp_server

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ def get_users(file_name):
             emails.append(user_info.split()[1])
     return names, emails
 
+
 def parse_template(file_name):
     with open(file_name, 'r') as msg_template:
         msg_template_content = msg_template.read()
     return Template(msg_template_content)
+
+
+def main():
+    names, emails = get_users('contacts.txt')
+    message_template = parse_template('message.txt')
+
+    smtp_server = smtplib.SMTP(host='host_address', port=port_here)
+    smtp_server.starttls()
+    smtp_server.login(my_mail, my_password)
