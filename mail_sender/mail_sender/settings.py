b1ce52401aa1b3a726dc58d03ef43d2c7c3bb258
karavanets <dront22rus@mail.ru>
2023-02-06 18:20:47 +0700

    add settings for send mail in settings.py

--- mail_sender/mail_sender/settings.py
+++ mail_sender/mail_sender/settings.py
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.ethereal.email'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "ewald.mcclure@ethereal.email"
+EMAIL_HOST_PASSWORD = "xErxFzKtrJZWggQHp8"
