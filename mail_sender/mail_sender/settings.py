fe95363218f0d66a2b9f40a7f98ddf75dd4b9cb7
karavanets <dront22rus@mail.ru>
2023-02-08 12:50:21 +0700

    add data about to email in settings.py

--- mail_sender/mail_sender/settings.py
+++ mail_sender/mail_sender/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.ethereal.email"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'waylon.prosacco@ethereal.email'
+EMAIL_HOST_PASSWORD = 'zDSeCVc9BZn2X3zhTu'
