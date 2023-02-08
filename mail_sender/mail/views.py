a4aa7fb85c90a2caca0c97f7f8e6901133f1e7a3
karavanets <dront22rus@mail.ru>
2023-02-08 18:13:56 +0700

    add call func 'template_loading'

--- mail_sender/mail/views.py
+++ mail_sender/mail/views.py
@@ -46,19 +46,18 @@ def send_emails(request):
             subject = request.POST.get('subject')
             email_from = settings.EMAIL_HOST_USER
             data_acquisition(request)
-            with open('mail_sender/mail/message.txt', 'r') as message_file:
-                message = Template(message_file.read())
-                with open('user_info.txt', 'r') as users_file:
-                    for user_info in users_file:
-                        email = [user_info.split()[0]]
-                        first_name = user_info.split()[1]
-                        last_name = user_info.split()[2]
-                        birthday = user_info.split()[3]
-                        personalized_message = message.substitute(USER_FIRST_NAME=first_name.title(),
-                                                                  USER_LAST_NAME=last_name.title(),
-                                                                  USER_BDAY=birthday)
-                        msg = EmailMessage(subject, personalized_message, email_from, email, connection=connection)
-                        msg.content_subtype = 'html'
-                        msg.send()
+            message = template_loading(request)
+            with open('user_info.txt', 'r') as users_file:
+                for user_info in users_file:
+                    email = [user_info.split()[0]]
+                    first_name = user_info.split()[1]
+                    last_name = user_info.split()[2]
+                    birthday = user_info.split()[3]
+                    personalized_message = message.substitute(USER_FIRST_NAME=first_name.title(),
+                                                              USER_LAST_NAME=last_name.title(),
+                                                              USER_BDAY=birthday)
+                    msg = EmailMessage(subject, personalized_message, email_from, email, connection=connection)
+                    msg.content_subtype = 'html'
+                    msg.send()
 
     return render(request, 'mail/send_emails.html')
